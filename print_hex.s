e18361d25aec748e4619de5fd57d54542837f835
Lassi Jääskeläinen <lassi@lassijaaskelainen.fi>
2020-05-30 14:58:32 +0300

    Work in progress, trying to make this work with AS syntax

--- print_hex.s
+++ print_hex.s
@@ -1,55 +1,67 @@
 .section .data
-  HEX_OUT: db '0x0000',0
+  HEX_OUT:
+    .int 0x0000
 
   .section .text
   .globl _start
   _start:
 
-mov dx, 0x1fb7    ; Set the value we want to print to dx
-call print_hex    ; Print the hex value
-jmp $             ; Hang once we're done
+movw $0x1fb7, %dx    # Set the value we want to print to dx
+call print_hex    # Print the hex value
 
-%include "print_string.s"
+.include "print_string.s"
 
-; Prints the value of DX as hex.
+# Prints the value of DX as hex.
 print_hex:
-pusha             ; save the register values to the stack for later
+push %rax
+push %rcx
+push %rdx
+push %rbx
+push %rbp
+push %rsi
+push %rdi            # save the register values to the stack for later
 
-mov cx,4          ; Start the counter: we want to print 4 characters
-                  ; 4 bits per char, so we're printing a total of 16 bits
+movw $4, %cx          # Start the counter: we want to print 4 characters
+                  # 4 bits per char, so were printing a total of 16 bits
 
 char_loop:
-dec cx            ; Decrement the counter
+dec %cx            # Decrement the counter
 
-mov ax,dx         ; copy bx into ax so we can mask it for the last chars
-shr dx,4          ; shift bx 4 bits to the right
-and ax,0xf        ; mask ah to get the last 4 bits
+movw %dx, %ax         # copy bx into ax so we can mask it for the last chars
+shr $4, %dx          # shift bx 4 bits to the right
+and $0xf, %ax        # mask ah to get the last 4 bits
 
-mov bx, HEX_OUT   ; set bx to the memory address of our string
-add bx, 2         ; skip the '0x'
-add bx, cx        ; add the current counter to the address
+movw HEX_OUT, %bx   # set bx to the memory address of our string
+add $2, %bx         # skip the '0x'
+add %cx, %bx        # add the current counter to the address
 
-cmp ax,0xa        ; Check to see if it's a letter or number
-jl set_letter     ; If it's a number, go straight to setting the value
-add byte [bx],7   ; If it's a letter, add 7
-                  ; Why this magic number? ASCII letters start 17
-                  ; characters after decimal numbers. We need to cover that
-                  ; distance. If our value is a 'letter' it's already
-                  ; over 10, so we need to add 7 more.
+cmp $0xa, %ax        # Check to see if its a letter or number
+jl set_letter     # If its a number, go straight to setting the value
+add $7, %bx   # If its a letter, add 7
+                  # Why this magic number? ASCII letters start 17
+                  # characters after decimal numbers. We need to cover that
+                  # distance. If our value is a letter its already
+                  # over 10, so we need to add 7 more.
 jl set_letter
 
 set_letter:
-add byte [bx],al  ; Add the value of the byte to the char at bx
+add %al, %bl  # Add the value of the byte to the char at bx
 
-cmp cx,0          ; check the counter, compare with 0
-je print_hex_done ; if the counter is 0, finish
-jmp char_loop     ; otherwise, loop again
+cmp 0, %cx          # check the counter, compare with 0
+je print_hex_done # if the counter is 0, finish
+jmp char_loop     # otherwise, loop again
 
 print_hex_done:
-mov bx, HEX_OUT   ; print the string pointed to by bx
+movw HEX_OUT, %bx   # print the string pointed to by bx
 call print_string
 
-popa              ; pop the initial register values back from the stack
-ret               ; return the function
+pop %rdi
+pop %rsi
+pop %rbp
+pop %rbx
+pop %rdx
+pop %rcx
+pop %rax              # pop the initial register values back from the stack
+ret               # return the function
 
-; global variables
+# global variables
